0fafcc9ed6ec0b4cad151701d8f4835a39e7b0aa
weesiau <siauwee@u.nus.edu>
2024-03-29 00:43:32 +0800

    Config jest for frontend testing

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
-module.exports = {
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
     clearMocks: true,
     preset: 'ts-jest',
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/prisma-mock.ts'],
+    moduleDirectories: ["node_modules", "<rootDir>/src/"],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+        'next-auth/(.*)$': '<rootDir>/node_modules/$1',
     },
+    transform: {
+        "^.+\\.(ts|js)x?$": "babel-jest",
+    },
+    moduleFileExtensions: ["js","jsx", "ts", "tsx"]
 }
+
+module.exports = createJestConfig(customJestConfig)
