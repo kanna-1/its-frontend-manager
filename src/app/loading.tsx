ff45cdbaebd37b5c422c7cfd41049ac31dc159d6
danielk0k <70556073+danielk0k@users.noreply.github.com>
2024-03-30 10:58:46 +0800

    Add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="text-center mt-64">
+      <svg
+        className="inline ml-2 animate-spin h-16 w-16 rounded-full border-4 border-solid border-current border-r-transparent"
+        viewBox="0 0 64 64"
+      ></svg>
+      <p className="mt-4">Loading ...</p>
+    </div>
+  );
+}
