a05acefc9fd57bb1d6dcea3275f9f45fead5e744
danielk0k <70556073+danielk0k@users.noreply.github.com>
2024-03-31 09:43:19 +0800

    Edit action button

--- src/components/grading/columns.tsx
+++ src/components/grading/columns.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
+import { ChevronRightCircle, MoreHorizontal } from "lucide-react";
 import EditFeedbackDialog from "../dialogs/editFeedback";
 import EditScoreDialog from "../dialogs/editScore";
 import { usePathname } from "next/navigation";
@@ -50,37 +50,16 @@ export const columns: ColumnDef<Users>[] = [
     accessorKey: "action",
     header: "Action",
     cell: ({ row }) => {
-      const email = row.original.email; // Extracting email from the row data
-      const id = row.original.id;
-      return <ActionDropdown email={email} submissionid={id} />;
+      return <MoreDetails submissionid={row.original.id} />;
     },
   },
 ];
 
-// Define a separate component for the dropdown menu
-const ActionDropdown: React.FC<{ email: string; submissionid: string }> = ({
-  email,
-  submissionid,
-}) => {
+function MoreDetails({ submissionid }: { submissionid: string }) {
   const pathname = usePathname();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Open menu</span>
-          <MoreHorizontal className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <Link href={pathname.replace("grading", submissionid)}>
-          <DropdownMenuItem>View code</DropdownMenuItem>
-        </Link>
-        {/* <div>
-          <EditScoreDialog submissionId={submissionid} />
-        </div>
-        <EditFeedbackDialog submissionId={submissionid} /> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Link href={pathname.replace("grading", submissionid)}>
+      <ChevronRightCircle className="text-muted-foreground" />
+    </Link>
   );
-};
+}
