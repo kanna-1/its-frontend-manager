71348c2673b6475469d1738cdcc1fa99d226b62f
Janssen <janssenlau88@gmail.com>
2024-04-03 20:00:30 +0800

    Update UX

--- src/components/dialogs/addMember.tsx
+++ src/components/dialogs/addMember.tsx
@@ -9,7 +9,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { z } from "zod";
+import { setErrorMap, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
@@ -24,15 +24,28 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Course, User } from "@prisma/client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const formSchema = z.object({
   emails: z.string()
 });
 
+
 export default function AddMemberDialog({ user, course }: { user: User, course: Course }) {
   const router = useRouter();
   const [open, setOpen] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  // Clears the success and error when dialog opens
+  useEffect(() => {
+    if (open) {
+      setError('');
+      setSuccess(false);
+    }
+  }, [open]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -42,6 +55,13 @@ export default function AddMemberDialog({ user, course }: { user: User, course: 
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
+      setError('');
+      setSuccess(false);
+      setSubmitting(true);
+      if (!values.emails.trim()) {
+        setError("Email field cannot be empty!")
+        return
+      }
       const res = await fetch("/api/course-management/add-to-course", {
         method: "POST",
         body: JSON.stringify({
@@ -53,18 +73,60 @@ export default function AddMemberDialog({ user, course }: { user: User, course: 
           "Content-Type": "application/json",
         },
       });
-      router.refresh()
-      setOpen(false)
+      const resBody = await res.json();
+      if (resBody.status == 'error') {
+        setError(resBody.message)
+      } else {
+        setSuccess(true)
+        setTimeout(() => {
+          setOpen(false);
+          router.refresh();
+        }, 2000); // 2-second delay
+      }
     } catch (error) {
       console.error(error);
+    } finally {
+      setSubmitting(false)
     }
   }
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+      <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
         <Button>Invite Member</Button>
       </DialogTrigger>
       <DialogContent>
+
+      {error && (
+        <p
+          style={{
+            backgroundColor: "#ffcccc",
+            fontWeight: "500",
+            color: "red",
+            padding: "10px",
+            borderRadius: "5px",
+            marginBottom: "10px",
+          }}
+        >
+          {error}
+        </p>
+      )}
+      {success && (
+        <p
+          style={{
+            backgroundColor: "#ccffcc",
+            fontWeight: "500",
+            color: "green",
+            padding: "10px",
+            textAlign: 'center',
+            borderRadius: "5px",
+            marginBottom: "10px",
+            fontSize: "10px",
+          }}
+        >
+          Student(s) successfully added!
+        </p>
+      )}
+
         <DialogHeader>
           <DialogTitle>Add new members</DialogTitle>
         </DialogHeader>
@@ -84,7 +146,9 @@ export default function AddMemberDialog({ user, course }: { user: User, course: 
                 </FormItem>
               )}
             />
-            <Button type="submit">Submit</Button>
+            <Button type="submit" disabled={submitting}>
+              {submitting ? 'Submitting...' : 'Submit'}
+            </Button>
           </form>
         </Form>
       </DialogContent>
