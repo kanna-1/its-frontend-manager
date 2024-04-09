22d307e98c6416c412f5169cc9aeec5e6c3015a3
weesiau <siauwee@u.nus.edu>
2024-04-09 17:33:06 +0800

    Update reset-password api

--- src/app/api/reset-password/route.ts
+++ src/app/api/reset-password/route.ts
@@ -2,7 +2,6 @@ import { hash } from 'bcryptjs';
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
-
 export async function POST(req: NextRequest) {
   try {
     const { password, token } = (await req.json()) as {
@@ -17,29 +16,33 @@ export async function POST(req: NextRequest) {
 
     if (resetToken) {
       const changePassword = await prisma.user.update({
-          where: {
-            email: resetToken.email,
-          },
-          data: {
-            password: hashed_password,
-          }
+        where: {
+          email: resetToken.email,
+        },
+        data: {
+          password: hashed_password,
+        }
       });
 
       return NextResponse.json({
         user: {
           email: resetToken.email
         },
+      }, {
+        status: 200
       });
     } else {
-      throw new Error("Null password reset token");
+      return NextResponse.json({
+        error: 'Invalid password reset token.'
+      }, {
+        status: 404
+      });
     }
   } catch (error: any) {
-    return new NextResponse(
-      JSON.stringify({
-        status: 'error',
-        message: error.message,
-      }),
-      { status: 500 }
-    );
+    return NextResponse.json({
+      error: error.message
+    }, {
+      status: 500
+    });
   }
 }
