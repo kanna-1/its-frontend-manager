cadbbbb8490712f4c3f3adbc29d0cd8e522864b3
Janssen <janssenlau88@gmail.com>
2024-04-17 02:47:27 +0800

    More refactoring

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,12 +1,12 @@
 import prisma from "@/lib/prisma";
 import { v4 as uuidv4 } from "uuid";
 
-export const createPasswordResetToken = async (email: string) : Promise<any> => {
+export const createPasswordResetToken = async (email: string): Promise<any> => {
     const token = uuidv4();
     const expires = new Date(new Date().getTime() + 3600 * 2000);
 
     const foundToken = await prisma.passwordResetToken.findFirst({
-        where : { email }
+        where: { email }
     })
 
     if (foundToken) {
--- src/prisma-mock.ts
+++ src/prisma-mock.ts
@@ -3,12 +3,12 @@ import { PrismaClient } from "@prisma/client"
 import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended"
 
 jest.mock("@/lib/prisma", () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
+    __esModule: true,
+    default: mockDeep<PrismaClient>(),
 }))
 
 beforeEach(() => {
-  mockReset(prismaMock)
+    mockReset(prismaMock)
 })
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
