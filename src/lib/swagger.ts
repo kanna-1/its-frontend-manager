d6973670b1130e5cafc8518cd2406dc522fa52f2
weesiau <siauwee@u.nus.edu>
2024-04-19 14:37:35 +0800

    Change print statement

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,7 +1,7 @@
 import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async (): Promise<object> => {
-  console.log(process.env.AUTH_SECRET)
+  console.log(process.env.PUBLIC_URL)
   const spec = createSwaggerSpec({
     apiFolder: "src/app/api", // define api folder under app folder
     definition: {
