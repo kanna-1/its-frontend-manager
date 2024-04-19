481d3a0cc2e5af2e2d8347c191c546fc2ec3185b
weesiau <siauwee@u.nus.edu>
2024-04-19 14:46:18 +0800

    Try

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -3,7 +3,8 @@ import { createSwaggerSpec } from "next-swagger-doc";
 export const getApiDocs = async (): Promise<object> => {
   console.log(process.env.PUBLIC_URL)
   const spec = createSwaggerSpec({
-    apiFolder: process.env.PUBLIC_URL + "src/app/api", // define api folder under app folder
+    // apiFolder: process.env.PUBLIC_URL + "src/app/api", // define api folder under app folder
+    apiFolder: `${__dirname}/src/app/api`,
     definition: {
       openapi: "3.0.0",
       info: {
