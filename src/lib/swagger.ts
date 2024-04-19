4916f91b3c60f4f64ecdf0995752aff938459784
weesiau <siauwee@u.nus.edu>
2024-04-19 14:40:31 +0800

    Add base url

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -3,7 +3,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 export const getApiDocs = async (): Promise<object> => {
   console.log(process.env.PUBLIC_URL)
   const spec = createSwaggerSpec({
-    apiFolder: "src/app/api", // define api folder under app folder
+    apiFolder: process.env.PUBLIC_URL + "src/app/api", // define api folder under app folder
     definition: {
       openapi: "3.0.0",
       info: {
