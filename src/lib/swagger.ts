a8afd9c19b0f904aeef5b2c213f5a887da7dc241
weesiau <siauwee@u.nus.edu>
2024-04-19 14:56:05 +0800

    Try 3

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,7 +1,6 @@
 import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async (): Promise<object> => {
-  console.log(process.env.PUBLIC_URL)
   const spec = createSwaggerSpec({
     // apiFolder: process.env.PUBLIC_URL + "src/app/api", // define api folder under app folder
     definition: {
@@ -11,8 +10,8 @@ export const getApiDocs = async (): Promise<object> => {
         description: `This documentation specifies the details of the API routes 
         that form the backend of the Management System for ITS.`,
         version: "1.0",
-        servers: [{ url: 'src/app/api' }]
       },
+      apis: ["src/app/api/*"],
     },
   });
   return spec;
